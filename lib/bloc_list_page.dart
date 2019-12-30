51aeef10bc4733ae0b18ccb09d6f62ee71030cbd
yao hongchao <472574996@qq.com>
2019-12-30 18:26:41 +0800

    map to UI

--- lib/bloc_list_page.dart
+++ lib/bloc_list_page.dart
@@ -3,8 +3,23 @@ import 'package:flutter_easyrefresh/easy_refresh.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'info_bloc.dart';
 import 'info_event.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'info_state.dart';
 
-class BlocListPage extends StatelessWidget{
+class BlocListPage extends StatefulWidget {
+  @override
+  _BlocListPageState createState() => _BlocListPageState();
+}
+
+class _BlocListPageState extends State<BlocListPage> {
+  InfoBloc _infoBloc;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _infoBloc = BlocProvider.of<InfoBloc>(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -12,8 +27,29 @@ class BlocListPage extends StatelessWidget{
       appBar: AppBar(
         title: Text("Movies"),
       ),
-      body: BlocProvider(create: (context) => InfoBloc()..add(RefreshEvent()),
-      child: EasyRefresh(child: null),),
+      body: BlocBuilder<InfoBloc, InfoState>(
+        builder: (context, state) {
+          if (state is InfoLoadedState) {
+            return EasyRefresh(
+              child: ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                return CachedNetworkImage(imageUrl: state.models[index].cover);
+              }),
+              onRefresh: () async {
+                _infoBloc.add(RefreshEvent());
+              },
+              onLoad: () async {
+                _infoBloc.add(LoadMoreEvent());
+              },
+              firstRefresh: true,
+            );
+          } else {
+            return Center(
+              child: Text("Unkown"),
+            );
+          }
+        },
+      ),
     );
   }
 }
